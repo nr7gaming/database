d0b45c9dfc58342f63482d37249724c39e3133bb
killa <killavulkan@nr7gaming.eu>
2011-10-04 17:06:52 +0200

    Add Sql Merge Tool Loot fix von RaR Mob: Kriegsgolem (http://de.wowhead.com/npc=2751#drops) SPAWN Object für die Quest: Gordunnikobalt (ID:2987)

---
+++ nR7-Updates/02_creature_loot.sql
@@ -0,0 +1,94 @@
+-- Loot fix von RaR Mob: Kriegsgolem (http://de.wowhead.com/npc=2751#drops)
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7542);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7542, 5, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7535);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7535, 4, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7530);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7530, 4, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7546);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7546, 3, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7541);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7541, 3, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9922);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9922, 3, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=10090);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 10090, 3, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7521);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7521, 3, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7523);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7523, 3, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7526);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7526, 2, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9956);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9956, 2, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7540);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7540, 1.7, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7543);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7543, 1.7, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=10088);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 10088, 1.7, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7532);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7532, 1.7, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9961);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9961, 1.7, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9959);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9959, 1.7, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9933);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9933, 1.6, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7545);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7545, 1.5, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9921);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9921, 1.4, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9966);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9966, 1.4, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7520);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7520, 1.4, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9960);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9960, 1.4, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=10091);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 10091, 1.3, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7538);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7538, 1.2, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9923);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9923, 1.2, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9924);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9924, 1.2, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9928);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9928, 1.1, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=10089);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 10089, 1.1, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=7519);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 7519, 1.1, 1, 1, 1, 0, 0, 0);
+
+DELETE FROM `creature_loot_template` WHERE (`entry`=2751) AND (`item`=9915);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES (2751, 9915, 1.1, 1, 1, 1, 0, 0, 0);
