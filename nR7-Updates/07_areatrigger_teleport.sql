b33f40fd32bada08e053cfacde0a1e2380582103
killa <killavulkan@nr7gaming.eu>
2011-10-09 17:51:11 +0200

    typo

--- nR7-Updates/07_areatrigger_teleport.sql
+++ nR7-Updates/07_areatrigger_teleport.sql
@@ -1,2 +1,2 @@
 -- Ausgang von MoltenCore
-UPDATE `areatrigger_teleport` SET `target_map`=0,`target_position_x`=-7511.47,`target_position_y`=-1042.2368,`target_position_z`=180.9121,`target_orientation`= 0.7152 WHERE `id` = 2890;
+UPDATE `areatrigger_teleport` SET `target_map`=0,`target_position_x`=-7511.47,`target_position_y`=-1042.2368,`target_position_z`=180.9121,`target_orientation`=0.7152 WHERE `id`=2890;
