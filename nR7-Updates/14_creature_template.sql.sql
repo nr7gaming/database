cbf205d7568a9cddaa14bc10a5cf289dff377386
killa <killavulkan@nr7gaming.eu>
2011-10-12 12:57:20 +0200

    NPC flag fix for pet trainers alliance (reset pet talents gossip menu) by Xenon and Brutus

---
+++ nR7-Updates/14_creature_template.sql.sql
@@ -0,0 +1,20 @@
+-- 14_creature_template.sql
+
+-- Belia Thundergranite
+UPDATE `creature_template` SET `npcflag`=19 WHERE `entry`=10090;
+
+-- Silvaria
+UPDATE `creature_template` SET `npcflag`=19 WHERE `entry`=10089;
+
+-- Keldas
+UPDATE `creature_template` SET `npcflag`=19 WHERE `entry`=3306;
+
+-- Karrina Mekenda
+UPDATE `creature_template` SET `npcflag`=19 WHERE `entry`=2879;
+
+-- Peria Lamenur
+UPDATE `creature_template` SET `npcflag`=19 WHERE `entry`=2878;
+
+-- Regnus Thundergranite (due to incorrect flag)
+UPDATE `creature_template` SET `npcflag`=19 WHERE `entry`=5117;
+
