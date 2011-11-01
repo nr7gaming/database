9f7e39ef76a0155c6305023d5c892c2b7a0e19d2
killa <killavulkan@nr7gaming.eu>
2011-11-01 21:38:57 +0100

    Quest: Aurius Medallion of Faith "CompleteEmote" and NPC: Aurius npcflag fix

---
+++ nR7-Updates/19_creature_template.sql
@@ -0,0 +1,5 @@
+-- NPC: Aurius
+UPDATE `creature_template` SET `gossip_menu_id`=3043, npcflag=3, `ScriptName`="" WHERE `entry`=10917;
+DELETE FROM `gossip_menu` WHERE `id`=3043;
+INSERT INTO `gossip_menu` VALUES ('3043', '3755', '0', '0', '0', '0', '0', '0');
+INSERT INTO `gossip_menu` VALUES ('3043', '3756', '0', '0', '0', '0', '0', '0');
