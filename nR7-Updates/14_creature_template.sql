b2e5e98d87caefbcb8662d589973a7912ebeedbe
nr7gaming <killavulkan@nr7gaming.eu>
2012-08-20 23:01:34 +0200

    alle vorhande Update die sich in Full_World befinden wurden gelöscht!

--- nR7-Updates/14_creature_template.sql
+++
@@ -1,5 +0,0 @@
--- Add Gossip zu NPC Tirion Fordring
-UPDATE `creature_template` SET `gossip_menu_id`=3502 WHERE `entry`=1855;
-INSERT INTO `gossip_menu` VALUES ('3502', '4254', '0', '0', '0', '0', '0', '0');
-DELETE FROM `npc_text` WHERE `ID`='4254';
-INSERT INTO `npc_text` VALUES ('4254', 'Race does not dictate honor, $r. While you remain on my farmstead, I ask that you remember and respect this credo.$B$BI have known orcs who have been as honorable as the most noble of knights and humans who have been as vile as the most ruthless of Scourge.$B$BBut I shall not bore you with tales of my youth! There is much work to be done - if that is what you desire. ', '', '0', '1', '0', '1', '0', '1', '0', '254', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0');
--- nR7-Updates/15_quest_template.sql
+++
@@ -1,2 +0,0 @@
--- Quest: Der vermisste Diplomat "Autocomplete"
-UPDATE `quest_template` SET `QuestFlags`=65536, `Method`=0 WHERE `entry`=1241;
