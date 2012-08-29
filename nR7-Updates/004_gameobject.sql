3c0c2a2c311fb25db2b183fd059ea8be28e192d0
nr7gaming <killavulkan@nr7gaming.eu>
2012-08-29 16:57:51 +0200

    fix object Matrix-Prägograph 3005-B

---
+++ nR7-Updates/004_gameobject.sql
@@ -0,0 +1,3 @@
+-- Matrix-Prägograph 3005-B Object gefixst (Gnomeregan)
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES
+(28683,142475,90,-562.086,125.454,-202.145,-3.12414,0,0,0.999962,-0.008727,3600,100,1);
